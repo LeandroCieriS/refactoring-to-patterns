ab2aa80b7d8fc6ebb5470b96f579e0ebd165b764
Leandro Cieri <lcieri@domingoalonsogroup.com>
2023-01-24 15:03:39 +0000

    Refactor: Compose method

--- RefactoringToPatterns/ComposeMethod/List.cs
+++ RefactoringToPatterns/ComposeMethod/List.cs
@@ -1,45 +1,51 @@
-using System;
-using System.Linq;
-using System.Web;
-
 namespace RefactoringToPatterns.ComposeMethod
 {
     public class List
     {
-
         private readonly bool _readOnly;
         private int _size;
-        private Object[] _elements;
+        private object[] _elements;
 
         public List(bool readOnly)
         {
             _readOnly = readOnly;
             _size = 0;
-            _elements = new Object[_size];
+            _elements = new object[_size];
         }
 
-        public void Add(Object element) {
-            if(!_readOnly) {
-                int newSize = _size + 1;
+        public void Add(object element)
+        {
+            if (_readOnly) return;
 
-                if(newSize > _elements.Length) {
-                    Object[] newElements = new Object[_elements.Length + 10];
+            if (IsFull()) ExpandSize();
 
-                    for (int i = 0; i < _size; i++)
-                        newElements[i] = _elements[i];
+            AddElement(element);
+        }
 
-                    _elements = newElements;
-                }
+        private void AddElement(object element)
+        {
+            _elements[_size++] = element;
+        }
 
-                _elements[_size++] = element;
-            }
+        private void ExpandSize()
+        {
+            var newElements = new object[_elements.Length + 10];
+
+            for (var i = 0; i < _size; i++)
+                newElements[i] = _elements[i];
+
+            _elements = newElements;
+        }
+
+        private bool IsFull()
+        {
+            var newSize = _size + 1;
+            return newSize > _elements.Length;
         }
 
         public object[] Elements()
         {
             return _elements;
         }
-
     }
-
 }
